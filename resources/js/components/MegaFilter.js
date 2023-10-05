6f4506e98a1a3794cd20fd306df878d2d751c258
Rafael Milewski <rafael.milewski@gmail.com>
2023-10-05 21:42:36 +0800

    add lens support

---
+++ resources/js/components/MegaFilter.js
@@ -0,0 +1,44 @@
+import reduce from 'lodash/reduce'
+
+export function filtersAreApplied(store, resourceName, filterFunction) {
+
+    const filters = store.getters[ `${ resourceName }/filters` ].filter(filterFunction)
+
+    const response = reduce(filters, (result, filter) => {
+        const originalFilter = store.getters[ `${ resourceName }/getOriginalFilter` ](filter.class)
+        const originalFilterCloneValue = JSON.stringify(originalFilter.currentValue)
+        const currentFilterCloneValue = JSON.stringify(filter.currentValue)
+        return currentFilterCloneValue === originalFilterCloneValue ? result : result + 1
+    }, 0)
+
+    console.log(filters, resourceName, response)
+
+    return response
+
+}
+
+export function withoutMegaFilter(filter) {
+    return filter.megaFilter !== true
+}
+
+export function megaFilterOnly(filter) {
+    return filter.megaFilter === true
+}
+
+export function registerMixin(component) {
+
+    const filters = component.computed.filters
+
+    component.computed.filters = function () {
+        return filters.call(this).filter(withoutMegaFilter)
+    }
+
+    component.computed.activeFilterCount = function () {
+        return filtersAreApplied(this.$store, this.resourceName, withoutMegaFilter)
+    }
+
+    component.computed.filtersAreApplied = function () {
+        return this.activeFilterCount > 0
+    }
+
+}
